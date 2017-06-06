9de09ec56468d7b4f92b5a40eed84039955488dd
Hieu Cao <qhieu45@gmail.com>
2017-06-06 16:43:25 +0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,15 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  let user = this;
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  })
+};
+
 
 
 // model method
